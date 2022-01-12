b06604df4e6ba71ee6bffb44b17d175441a260e6
KabutoDrip <cammiller42003@gmail.com>
2022-01-12 15:32:52 -0500

    pre major changes

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,32 +2,54 @@
 #Camden Miller
 #1/12/22
 import random
-def create_grid(user_input,bot_input):
-    l1,c1,r1 = 1,2,3 
-    l2,c2,r2 = 4,5,6
-    l3,c3,r3 = 7,8,9
-    return l1,c1,r1,l2,c2,r2,l3,c3,r3
+def show_grid(l1,l2,l3,l4,l5,l6,l7,l8,l9):
+    line1 = (f"{l1}|{l2}|{l3}")
     
-
+    line2 = (f"{l4}|{l5}|{l6}")
     
-def show_grid(l1,c1,r1,l2,c2,r2,l3,c3,r3):
-    line1 = (f"{l1}|{c1}|{r1}")
-    
-    line2 = (f"{l2}|{c2}|{r2}")
-    
-    line3 = (f"{l3}|{c3}|{r3}")
+    line3 = (f"{l7}|{l8}|{l9}")
     
     XsandOs_board = (f"{line1}\n-+-+-\n{line2}\n-+-+-\n{line3}")
     print (XsandOs_board)
 def user_input():
-    print()
+    user_inputed = input("enter a number based on where you want to go ")
+    return user_inputed
 def bot_input():
     print()
-def main(l1,c1,r1,l2,c2,r2,l3,c3,r3):
-    order = random.randint(0,1)
+def edit_grid(user_input):
+    if user_input == "1":
+        global l1
+        l1 = "X"
+    if user_input == 2:
+        global l2
+        l2 = "X"
+    if user_input == 3:
+        global l3
+        l3 = "X"        
+    if user_input == 4:
+        global l4
+        l4 = "X"
+    if user_input == 5:
+        global l5
+        l5 = "X"
+    if user_input == 6:
+        global l6
+        l6 = "X"
+    if user_input == 7:
+        global l7
+        l7 = "X"
+    if user_input == 8:
+        global l8
+        l8 = "X"
+    if user_input == 9:
+        global l9
+        l9 = "X"
+def main(l1,l2,l3,l4,l5,l6,l7,l8,l9):
+    order = random.randint(1,1)
     if order == 1:
+        show_grid(l1,l2,l3,l4,l5,l6,l7,l8,l9)
         user_input()
-        show_grid(l1,c1,r1,l2,c2,r2,l3,c3,r3)
+        edit_grid(user_input)
         bot_input()
-    create_grid(user_input,bot_input)
-main(1,2,3,4,5,6,7,8,9)
+        show_grid(l1,l2,l3,l4,l5,l6,l7,l8,l9)
+main(l1,l2,l3,l4,l5,l6,l7,l8,l9)
