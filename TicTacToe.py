8efc3e84a487c22643547088f794dad76dbb16fe
KabutoDrip <cammiller42003@gmail.com>
2022-01-12 22:21:10 -0500

    functional bot and player no win state or error handling

--- TicTacToe.py
+++ TicTacToe.py
@@ -16,9 +16,12 @@ def selection():
     x_or_o = input("would you like to play as (x) or (o)")
     if x_or_o == "x":
         global token
+        global bot_token
         token = "X"
+        bot_token = "O"
     elif x_or_o =="o":
         token = "O"
+        bot_token = "X"
     else:
         print("You messed up so you will play as O")
     if random.randint(0,1) == 0:
@@ -33,12 +36,37 @@ def game_state():
     if pos[1] == pos[2] == pos[3]:
         game = "over"
     return game
-def turn(game_state):
-    gameis = game_state()
-    location = int(input("where would you like to go(1-9)"))
-    pos[location-1] = token
-def main():
+def turn():
+    while True:
+        location = int(input("where would you like to go(1-9)"))
+        if pos[location -1] != "X" and pos[location -1] != "O":
+            pos[location-1] = token
+            break;
+        else:
+            print("Enter a location that hasnt been used")
+            continue
+def bot():
+    location = random.randint(0,8)
+    pos[location] = bot_token
+def main(game_state):
     selection()
-    turn(game_state)
-    show_grid(pos)
-main()
+    game = game_state()
+    if first == True:
+        show_grid(pos)
+        while game != "over":
+            turn()
+            show_grid(pos)
+            print()
+            bot()
+            show_grid(pos)
+            print()
+    else:
+        show_grid(pos)
+        while game != "over":
+            bot()
+            show_grid(pos)
+            print()
+            turn()
+            show_grid(pos)
+            print()
+main(game_state)
