3f383ba874a0cb9d962e706f7b880bc251c37425
KabutoDrip <cammiller42003@gmail.com>
2022-01-12 21:57:30 -0500

    reworked to make more sense

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,51 +2,43 @@
 #Camden Miller
 #1/12/22
 import random
-
-def show_grid(l1,l2,l3,l4,l5,l6,l7,l8,l9):
-    line1 = (f"{l1}|{l2}|{l3}")
+pos = [1,2,3,4,5,6,7,8,9]
+def show_grid(pos):
+    line1 = (f"{pos[0]}|{pos[1]}|{pos[2]}")
     
-    line2 = (f"{l4}|{l5}|{l6}")
+    line2 = (f"{pos[3]}|{pos[4]}|{pos[5]}")
     
-    line3 = (f"{l7}|{l8}|{l9}")
+    line3 = (f"{pos[6]}|{pos[7]}|{pos[8]}")
     
     XsandOs_board = (f"{line1}\n-+-+-\n{line2}\n-+-+-\n{line3}")
     print (XsandOs_board)
-def user_input():
-    user_inputed = int(input("enter a number based on where you want to go "))
-    return user_inputed
-def bot_input():
-    print()
-def edit_grid(user_input,l1,l2,l3,l4,l5,l6,l7,l8,l9):
-    if user_input == 1:
-        l1 = "X"
-    if user_input == 2:
-        l2 = "X"
-    if user_input == 3:
-        l3 = "X"        
-    if user_input == 4:
-        l4 = "X"
-    if user_input == 5:
-        l5 = "X"
-    if user_input == 6:
-        l6 = "X"
-    if user_input == 7:
-        l7 = "X"
-    if user_input == 8:
-        l8 = "X"
-    if user_input == 9:
-        l9 = "X"
+def selection():
+    x_or_o = input("would you like to play as (x) or (o)")
+    if x_or_o == "x":
+        global token
+        token = "X"
+    elif x_or_o =="o":
+        token = "O"
     else:
-        l1 = "X" 
-    print(l1,l2,l3,l4,l5,l6,l7,l8,l9)
-    return l1,l2,l3,l4,l5,l6,l7,l8,l9
-
+        print("You messed up so you will play as O")
+    if random.randint(0,1) == 0:
+        global first
+        first = True
+        
+    else:
+        first = False
+    print (token,first)
+def game_state():
+    game = ""
+    if pos[1] == pos[2] == pos[3]:
+        game = "over"
+    return game
+def turn(game_state):
+    gameis = game_state()
+    location = int(input("where would you like to go(1-9)"))
+    pos[location-1] = token
 def main():
-    order = random.randint(1,1)
-    if order == 1:
-        l1,l2,l3,l4,l5,l6,l7,l8,l9 = 1,2,3,4,5,6,7,8,9
-        show_grid("1","2","3","4","5","6","7","8","9")
-        user_input()
-        l1,l2,l3,l4,l5,l6,l7,l8,l9 = edit_grid(user_input,l1,l2,l3,l4,l5,l6,l7,l8,l9)
-        show_grid(l1,l2,l3,l4,l5,l6,l7,l8,l9)
+    selection()
+    turn(game_state)
+    show_grid(pos)
 main()
